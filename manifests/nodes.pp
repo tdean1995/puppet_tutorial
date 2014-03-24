d45913133252fa0a15d567a48d1ba6dad8ebb32d
Tony Dean <tdean_95@yahoo.com>
2014-03-23 22:58:06 -0400

    add exec cmd

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -10,6 +10,10 @@ node 'dean-ubu' {
 }
 
 node 'dean-aws1' {
+  exec { 'Run my arbitrary command':
+    command => 'bin/echo I ran this command on `/bin/date` > /tmp/command.output.txt',
+  }
+
   include ssh
   include sudoers
   user { 'art':
