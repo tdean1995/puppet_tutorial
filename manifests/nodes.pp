d4d5493e55593380f9c7700590ef9728476cae68
Tony Dean <tdean_95@yahoo.com>
2014-03-23 20:39:58 -0400

    add user account

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,25 @@
 node 'dean-ubu' {
   include nginx
+  user { 'art':
+   ensure => present,
+   comment => 'Art Vanderlay',
+   home => '/home/art',
+   managehome => true,
+ } 
+
+}
+
+node 'dean-aps1' {
+  user { 'art':
+    ensure => present,
+    comment => 'Art Vanderlay'
+    home => '/home/art'
+    managehome => true,
+  }
+  ssh_authorized_key { 'art_ssh':
+    user => 'art',
+    type => 'rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDB3a6ha0D2kh/yrjsoel3Fa8rJtA2UYZQ1j2lwfobjPRFjIs2b6XNhcUh52RWsBartayix5m0dLEamiN12h+RoQ8j4nO89J62hQYatAhf9SYmgZlpzYG3aYLMl2incTbhUlZvyVwSd4UUZY5xCu4m0YbY2HCIlQyXLjzrAoHlIjzRCNDBYZwNUDkxPQGbOEgdf6iMhY8ra80DheGVLsEum3jxwuvG9vTFWNq2s2YEydXDq6oPmy1jj/YGi1AjGffOXnJ4RdCgi/4klI1JPWETCHmEGDOagTQ38Eiy98hC53qE3YJiL/nLk+Id8gjFOOnDC8GRvMyqUU0+GsWABCUIH',
+
+}
 }
