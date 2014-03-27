db43318534fb989116800d4241cafc304ca6139a
Tony Dean <tdean_95@yahoo.com>
2014-03-26 22:07:26 -0400

    add auto update job

--- modules/cron-update/manifests/init.pp
+++ modules/cron-update/manifests/init.pp
@@ -3,7 +3,7 @@
 
 
 cron { 'Update puppet repo':
-  command => '/usr/bin/git pull && sudo /usr/bin/puppet apply /etc/puppet/manifests/site.pp',
+  command => '/usr/bin/git pull && sudo /usr/bin/puppet apply /etc/puppet/manifests/site.pp --modulepath=/etc/puppet/modules',
   minute => '*/15',
   user => 'wdean', 
 }
