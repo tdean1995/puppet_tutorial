fd3864dffc257f1a4621c621ce0137d21f2f5743
Tony Dean <tdean_95@yahoo.com>
2014-04-10 22:34:58 -0400

    fix

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -21,23 +21,5 @@ class nginx {
   }
 
   
-  file { 'default':
-      path => '/etc/nginx/sites-enabled/default',
-      source => 'puppet:///modules/nginx/cat-pictures.conf',
-      ensure => file,
-      notify => Service['nginx'],
-  }
-  file {'cat-pictures':
-     path => '/var/www/html/cat-pictures',
-     ensure => directory,
-     notify => File['index.html']
-  }
-
-  file { 'index.html':
-	path => '/var/www/html/index.html',
-	source => 'puppet:///modules/nginx/cat-pic-index.html',
-        ensure => file,
-	notify => Service['nginx'],
-  }
 } 
  
